6a2f195561989dea277a5c166a7e795d1b0e96d6
David Gavilan <david@endavid.com>
2025-06-19 14:52:05 +0100

    Metal multiplatform template

---
+++ SampleBasic/SampleBasic macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  SampleBasic macOS
+//
+//  Created by David Gavilan Ruiz on 19/06/2025.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
