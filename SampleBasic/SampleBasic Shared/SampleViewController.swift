36ac180b967b625e4436fe0065f10af1349ae308
David Gavilan <david@endavid.com>
2025-06-20 17:14:35 +0100

    Add some sample text and debug the font texture

--- SampleBasic/SampleBasic Shared/SampleViewController.swift
+++ SampleBasic/SampleBasic Shared/SampleViewController.swift
@@ -19,6 +19,7 @@ class SampleViewController: VidController {
         cube.queue(renderer)
         self.cube = cube
         spawnPlanes(count: 5)
+        initTextDemo()
     }
     
     func spawnPlanes(count: Int) {
@@ -31,6 +32,30 @@ class SampleViewController: VidController {
             plane.instances[i].transform.position = [0, 0, -4.5 - 0.5 * Float(i)]
         }
         plane.queue(renderer)
+    }
+    
+    func initTextDemo() {
+        let fontName = "HoeflerText-Regular"
+        guard let font = VidFont(name: fontName, size: 72) else {
+            NSLog("Font not found: \(fontName)")
+            return
+        }
+        if let fontAtlas = try? FontAtlas.createFontAtlas(font: font, textureSize: 2048, archive: false) {
+            let makeItStand = Quaternion(AngleAxis(angle: .pi / 2, axis: simd_float3(1,0,0)))
+            let tiltToOneSide = Quaternion(AngleAxis(angle: .pi / 4, axis: simd_float3(0,1,0)))
+            let prim = TextPrimitive(instanceCount: 1, font: fontAtlas, text: "Hello World! :)", fontSizeMeters: 1, enclosingFrame: CGRect(x: -2, y: -5, width: 4, height: 10))
+            prim.transform.position = simd_float3(0,0,-22)
+            prim.transform.rotation = tiltToOneSide * makeItStand
+            prim.queue(renderer)
+            // debug the font atlas
+            let debugPanel = PlanePrimitive(instanceCount: 1)
+            debugPanel.lightingType = .unlitTransparent
+            debugPanel.transform = Transform(position: simd_float3(-4, 2, -20), scale: simd_float3(4,1,4), rotation: makeItStand)
+            debugPanel.albedoTexture = try? fontAtlas.getFontTexture(renderer)
+            debugPanel.queue(renderer)
+        } else {
+            NSLog("Error initializing FontAtlas")
+        }
     }
     
     override func update(_ elapsed: TimeInterval) {
