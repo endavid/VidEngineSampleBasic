ec1559e2c5427506760b259610385f9f2f0e163e
David Gavilan <david@endavid.com>
2025-06-20 15:35:49 +0100

    Add some transparent planes

--- SampleBasic/SampleBasic Shared/SampleViewController.swift
+++ SampleBasic/SampleBasic Shared/SampleViewController.swift
@@ -14,10 +14,23 @@ class SampleViewController: VidController {
     
     override func onAppear() {
         let cube = CubePrimitive(renderer: renderer, instanceCount: 1)
-        cube.lightingType = .UnlitOpaque
+        cube.lightingType = .unlitOpaque
         cube.transform.position = [0, 0, -5]
         cube.queue(renderer)
         self.cube = cube
+        spawnPlanes(count: 5)
+    }
+    
+    func spawnPlanes(count: Int) {
+        let plane = PlanePrimitive(instanceCount: count)
+        plane.material.diffuse = LinearRGBA(r: 0.5, g: 1, b: 1, a: 0.5)
+        plane.lightingType = .unlitTransparent
+        plane.transform.scale = [2, 1, 1]
+        plane.transform.rotation = Quaternion(AngleAxis(angle: 1.57, axis: normalize([1, 0, 0])))
+        for i in 0..<count {
+            plane.instances[i].transform.position = [0, 0, -4.5 - 0.5 * Float(i)]
+        }
+        plane.queue(renderer)
     }
     
     override func update(_ elapsed: TimeInterval) {
